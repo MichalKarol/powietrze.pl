de363512e779d39b44d29ad7af7ff1693c19d68a
Michal Karol <michal.p.karol@gmail.com>
2019-11-21 23:17:11 +0100

    Updates

---
+++ precache-manifest.6a22b7a9bf17a4bfd4114b247042f047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edc9a3241035bb36a0c1dab35b04205",
+    "url": "/powietrze/index.html"
+  },
+  {
+    "revision": "a9e5b5969f58dd738e24",
+    "url": "/powietrze/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "1ce57a1d228360bd39b8",
+    "url": "/powietrze/static/css/main.5049a8da.chunk.css"
+  },
+  {
+    "revision": "a9e5b5969f58dd738e24",
+    "url": "/powietrze/static/js/2.616c8540.chunk.js"
+  },
+  {
+    "revision": "1ce57a1d228360bd39b8",
+    "url": "/powietrze/static/js/main.878874a0.chunk.js"
+  },
+  {
+    "revision": "f1fa32caab2aaa0a4fcb",
+    "url": "/powietrze/static/js/runtime-main.67bc7ad8.js"
+  }
+]);
